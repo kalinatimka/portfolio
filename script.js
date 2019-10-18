48766b6ce218f3238d1694416f0ead60cbe13d7b
Timofey <kalinata0799@gmail.com>
2019-10-18 10:17:37 +0300

    feat: add expansion panel

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+var expansionPanel = document.querySelector('.education--wrapper');
+var schoolTime = document.querySelector('.education--time');
+var arrow = document.querySelector('.education--arrow');
 
+expansionPanel.addEventListener('click', function() {
+  schoolTime.classList.toggle('education--time-active');
+  arrow.classList.toggle('education--arrow-rotate');
+})
